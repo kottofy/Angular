34d71d385376d04184d629b82ac6c81ba22c4872
Kristin <kottofy@gmail.com>
2015-10-09 12:14:47 -0400

    tutorial unit one complete, added controller, directives, displays gems, hides add to cart button if not for sale, hides gem if product is sold out

---
+++ Angular/AngularBootstrapPractice/app.js
@@ -0,0 +1,25 @@
+﻿
+(function () {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+
+    var gems = [
+    {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'blah dee blah',
+        canPurchase: false,
+        soldOut: true
+    },
+    {
+        name: "Pentagonal Gem",
+        price: 5.95,
+        description: "pretty",
+        canPurchase: true,
+        soldOut: false
+    }
+     ]
+})();
