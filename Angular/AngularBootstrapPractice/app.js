34c1a7f723aef5b677076e5b80e432c4ea9d9b3c
Kristin <kottofy@gmail.com>
2015-10-09 18:46:32 -0400

    added custom directives and controllers

--- Angular/AngularBootstrapPractice/app.js
+++ Angular/AngularBootstrapPractice/app.js
@@ -62,16 +62,7 @@
     }
      ]
 
-    app.controller('PanelController', function () {
-        this.tab = 1;
 
-        this.selectTab = function (setTab) {
-            this.tab = setTab;
-        };
-        this.isSelected = function (checkTab) {
-            return this.tab === checkTab;
-        };
-    });
 
     app.controller('ReviewController', function () {
         this.review = {};
@@ -90,5 +81,45 @@
             templateUrl: 'product-title.html'
         };
     });
+
+    //app.directive('productGallery', function () {
+    //    return {
+    //        restrict: 'E', //new element
+    //        templateUrl: 'product-gallery.html',
+    //        controller: function () {
+    //            this.current = 0;
+    //            this.setCurrent = function (imageNumber) {
+    //                this.current = imageNumber || 0;
+    //            };
+    //        },
+    //        controllerAs: 'gallery'
+    //    };
+    //});
+
+    app.directive('productDescription', function () {
+        return {
+            restrict: 'E', //new element
+            templateUrl: 'product-description.html'
+        };
+    });
+
+    app.directive('productPanels', function () {
+        return {
+            restrict: 'E', //new element
+            templateUrl: 'product-panels.html',
+            controller: function () {
+                this.tab = 1;
+
+                this.selectTab = function (setTab) {
+                    this.tab = setTab;
+                };
+                this.isSelected = function (checkTab) {
+                    return this.tab === checkTab;
+                };
+            },
+            controllerAs: 'panels'
+        };
+    });
+
 })();
 
