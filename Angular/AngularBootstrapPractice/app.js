3e67a7bd88556f6af9c8e3729cf8a8ff484e9bbd
Kristin <kottofy@gmail.com>
2015-10-09 17:08:08 -0400

    added review controller

--- Angular/AngularBootstrapPractice/app.js
+++ Angular/AngularBootstrapPractice/app.js
@@ -74,7 +74,12 @@
     app.controller('ReviewController', function () {
         this.review = {};
 
-    })
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+
+    });
 
 })();
 
