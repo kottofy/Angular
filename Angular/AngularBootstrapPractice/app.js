ba3144c9db8844604b4fc558cf34c5cb877278df
Kristin <kottofy@gmail.com>
2015-10-09 17:00:24 -0400

    reviews displayed with add a review form

--- Angular/AngularBootstrapPractice/app.js
+++ Angular/AngularBootstrapPractice/app.js
@@ -18,6 +18,18 @@
                 full: 'images/full.png',
                 thumb: 'images/thumb.png'
             }
+        ],
+        reviews: [
+            {
+                stars: 5,
+                body: "I love this product!",
+                author: "joe@thomas.com"
+            },
+            {
+                stars: 1,       
+                body: "This product sucks",
+                author: "tim@hater.com"
+            }
         ]
     },
     {
@@ -31,7 +43,38 @@
                 full: 'images/full.png',
                 thumb: 'images/thumb.png'
             }
+        ],
+        reviews: [
+                {
+                    stars: 3,
+                    body: "meh",
+                    author: "idc@meh.com"
+                },
+                {
+                    stars: 5,
+                    body: "This product is bananas",
+                    author: "bananas@lover.com"
+                }
         ]
+    
     }
      ]
+
+    app.controller('PanelController', function () {
+        this.tab = 1;
+
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+
+    app.controller('ReviewController', function () {
+        this.review = {};
+
+    })
+
 })();
+
