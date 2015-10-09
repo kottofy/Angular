fcff2ca880aef851814d885ace45799f6627b840
Kristin <kottofy@gmail.com>
2015-10-09 12:36:29 -0400

    added images to gems

--- Angular/AngularBootstrapPractice/app.js
+++ Angular/AngularBootstrapPractice/app.js
@@ -12,14 +12,26 @@
         price: 2.95,
         description: 'blah dee blah',
         canPurchase: false,
-        soldOut: true
+        soldOut: true,
+        images: [
+            {
+                full: 'images/full.png',
+                thumb: 'images/thumb.png'
+            }
+        ]
     },
     {
         name: "Pentagonal Gem",
         price: 5.95,
         description: "pretty",
         canPurchase: true,
-        soldOut: false
+        soldOut: false,
+        images: [
+            {
+                full: 'images/full.png',
+                thumb: 'images/thumb.png'
+            }
+        ]
     }
      ]
 })();
