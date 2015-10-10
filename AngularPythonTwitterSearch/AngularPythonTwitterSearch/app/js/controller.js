297dfb6da166797cea2a07d2d14d0d947ba9608f
kottofy <kottofy@gmail.com>
2015-10-10 17:20:57 -0400

    deleted old project

---
+++ AngularPythonTwitterSearch/AngularPythonTwitterSearch/app/js/controller.js
@@ -0,0 +1,28 @@
+﻿
+
+(function () {
+
+    var app = angular.module('searchTwitter', []);
+
+    app.controller('FormController', function () {
+
+        this.query = null;
+
+        this.query = function setQuery(query) {
+            this.query = query;
+        }();
+
+    });
+
+
+    app.directive('searchform', function () {
+        return {
+            restrict: 'E', //new element
+            templateUrl: '../forms/searchform.html'
+        };
+    });
+
+
+
+}());
+
