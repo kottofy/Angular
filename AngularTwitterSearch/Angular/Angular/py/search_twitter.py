5a848b2ae703cd7805d0aab35c002da1a792d4f4
kottofy <kottofy@gmail.com>
2015-10-10 13:55:50 -0400

    started to add python

---
+++ AngularTwitterSearch/Angular/Angular/py/search_twitter.py
@@ -0,0 +1,10 @@
+﻿
+
+
+
+def oauth_req(url, key, secret, http_method="GET", post_body=””, http_headers=None):
+    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+    token = oauth2.Token(key=key, secret=secret)
+    client = oauth2.Client(consumer, token)
+    resp, content = client.request( url, method=http_method, body=post_body, headers=http_headers )
+    return content
