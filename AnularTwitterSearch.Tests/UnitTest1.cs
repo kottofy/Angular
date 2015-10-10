739c9fb74da9abee071d8b7d2627fc84ece80dc7
kottofy <kottofy@gmail.com>
2015-10-10 10:57:31 -0400

    initial

---
+++ AnularTwitterSearch.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnularTwitterSearch.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
