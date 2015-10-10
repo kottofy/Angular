0d0ad48da2fcb2bf87c826f793f59fa39adbe874
kottofy <kottofy@gmail.com>
2015-10-10 13:57:33 -0400

    added oauth2 function

--- AngularTwitterSearch/Angular/Angular/py/search_twitter.py
+++ AngularTwitterSearch/Angular/Angular/py/search_twitter.py
@@ -1,7 +1,4 @@
 ﻿
-
-
-
 def oauth_req(url, key, secret, http_method="GET", post_body=””, http_headers=None):
     consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
     token = oauth2.Token(key=key, secret=secret)
