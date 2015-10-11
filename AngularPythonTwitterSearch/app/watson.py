297da8a53460b68fb6636ffee37bf3aba602ba2c
kottofy <kottofy@gmail.com>
2015-10-11 18:48:22 -0400

    Successfully getting sentiment analysis scores!!!! Now accepting pats on the back :)

--- AngularPythonTwitterSearch/app/watson.py
+++ AngularPythonTwitterSearch/app/watson.py
@@ -18,53 +18,26 @@ def get_sentiment_analysis(text):
     #print("getting sentiment analysis on: " + text.encode(encoding='UTF-8',errors='strict'))
 
     try:
-        # make a string with the request type in it:
-        #method = "POST"
-        # create a handler. you can specify different handlers here (file uploads etc)
-        # but we go for the default
-        #handler = urllib.HTTPHandler()
-        ## create an openerdirector instance
-        #opener = urllib.build_opener(handler)
-        # build a request
-        try:
-            request = urllib.request.urlopen(timeline_endpoint, data = urllib.parse.urlencode(data_dictionary).encode("utf-8"))
-            print('got request')    
-        except urllib.error.ContentTooShortError as e:
-            print(e)
-            print('Content Too Short')
-        except urllib.error.HTTPError as p:
-            print(p)
-            print('HTTP error')
-        except urllib.error.URLError as q:
-            print(q)
-            print('url error')
-        except Exception as m:
-            print(m)
-            print('caught exception!')
-        # add any other information you want
-        #request.add_header("Content-Type",'application/json')
-        ## overload the get method function with a small anonymous function...
-        #request.get_method = lambda: method
-        # try it; don't forget to catch the result
-        #try:
-        #    connection = opener.open(request)
-        #except urllib2.HTTPError as e:
-        #    connection = e
+        # send POST request to url with encoded data
+        response = urllib.request.urlopen(timeline_endpoint, data = urllib.parse.urlencode(data_dictionary).encode("utf-8"))
 
-        # check. Substitute with appropriate HTTP code.
-        #if connection.code == 200:
-        #    data = connection.read()
-        #else:
-        #    # handle the error case. connection.read() will still contain data
-        #    # if any was returned, but it probably won't be of any use
-        #    pass
+        # read the response - gets bytes
+        response_info = response.read()
 
-       
-        #response, data = client.request(timeline_endpoint)
-        #scores = json.loads(data.decode(encoding='UTF-8'))
-    except:
-        print("Something went wrong getting score")
+        #convert response to json
+        readable_info_json = json.loads(response_info.decode(encoding='UTF-8'))
+
+        #identify sentiment score!
+        score = readable_info_json['docSentiment']['score']
+
+        #big hurrah!
+        print('got score:' + readable_info_json['docSentiment']['score'])
+           
+        scores.append(score)
+
+    except Exception as m:
+        print('Error in getting Sentient Analysis score!')
+        print(m)      
         pass
-
 
     return scores
